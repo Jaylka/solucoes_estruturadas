a34f689d634b77c0e775c8aa5c9b55150055c320
Jaylka Cristina <165946877+Jaylka@users.noreply.github.com>
2024-11-15 20:48:51 +0000

    esboço do conceito de modularização

---
+++ #calcul_media_temp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Função entrada de dados
+void entradaDados() {
+    // código para a função entradadeDados
+}
+
+// Função calcular média
+float calcularMedia(){
+     // escrever todo calcul, nessa função
+}
+// Função para exibir a média
+void exibirMedia(){
+    // código para a func.exibirMedia
+}
+int main(){
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+}
